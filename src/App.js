37be678e57a896b9b6e6ab16cca42188cdd27a6b
PrabhjotDhanoa <dhanoa438@gmail.com>
2021-02-22 13:13:26 -0500

    Main Page UI

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Container from '@material-ui/core/Container';
+import AppBar from '@material-ui/core/AppBar';
+import SearchAppBar from './components/navbar';
+import CarouselImages from './components/carousal';
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container style={{ padding: '0px' }}>
+      <SearchAppBar />
+      <CarouselImages />
+    </Container>
   );
 }
 
